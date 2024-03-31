13a58585dadc3ecd4ee632b02fc2fe77d5af4661
IsabelGR <grisabel97@outlook.com>
2024-03-31 19:11:31 +0200

    feat/M-1.2: change styles

--- src/components/Home/Menu/components/Logo/Logo.tsx
+++ src/components/Home/Menu/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import { component$, $ } from "@builder.io/qwik";
 import styles from "./Logo.module.scss";
+import ImgLogo from "/logo.ico";
 
 const Logo = component$(() => {
   return (
     <div class={styles.logo}>
-      <img src="/logo.ico" width={150} height={80} alt="Logo Icon" />
+      <img src={ImgLogo} width={150} height={80} alt="Logo Icon" />
       <div class={styles.logoTitle}>
         <p class={styles.titleBold}>SMART</p>
         <p class={styles.titleThin}>SAVINGS</p>
