d981fa458f2f8bbb11ae4e5fe7c29364c4b0d72f
Delgado Trueba <jose.delgado@babel.es>
2024-04-07 20:18:13 +0200

    feat/features: left component

--- src/components/Home/Features/components/FeatureImgLeft/FeatureImgLeft.tsx
+++ src/components/Home/Features/components/FeatureImgLeft/FeatureImgLeft.tsx
@@ -1,23 +1,24 @@
 import { component$, PropFunction  } from "@builder.io/qwik";
 
-import styles from "./FeatureImgRight.module.scss";
+import styles from "./FeatureImgLeft.module.scss";
 
-export interface FeatureImgRightProps {
+export interface FeatureImgLeftProps {
   imgSrc: string;
   altImg: string;
   title: string;
   description: string;
 }
 
-export const FeatureImgRight = component$((props: FeatureImgRightProps) => {
+export const FeatureImgLeft = component$((props: FeatureImgLeftProps) => {
 
   return (
     <div class={styles.feature}>
+      <img class={styles.img} width={240} height={240}  src={props.imgSrc} alt={props.altImg} />
         <div class={styles.featureData}>      
             <h3 class={styles.title}>{props.title}</h3>
             <p class={styles.description}>{props.description}</p>
         </div>
-        <img class={styles.img} width={240} height={240}  src={props.imgSrc} alt={props.altImg} />
+        
     </div>
   );
 });
