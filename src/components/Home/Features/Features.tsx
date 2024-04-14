73b867d915270445c548ef64c8cf957aca59ef13
IsabelGR <grisabel97@outlook.com>
2024-04-14 13:14:38 +0200

    feat/features: change translations

--- src/components/Home/Features/Features.tsx
+++ src/components/Home/Features/Features.tsx
@@ -1,9 +1,8 @@
 import { component$ } from "@builder.io/qwik";
+import i18n from "~/i18n";
 
 import styles from "./Features.module.scss";
-import { en } from "~/i18n/en";
-import { es } from "~/i18n/es";
-import { useLanguage } from "~/hooks/useLanguage";
+
 import { FeatureImgRight } from "./components/FeatureImgRight/FeatureImgRight";
 import ImgDetails from "/images/details.png";
 import ImgSummary from "/images/summary.png";
@@ -12,36 +11,34 @@ import ImgTools from "/images/tools.png";
 import { FeatureImgLeft } from "./components/FeatureImgLeft/FeatureImgLeft";
 
 export const Features = component$(() => {
-  const { lang } = useLanguage();
-  const i18n = lang === "en" ? en : es;
-
+  const { t } = i18n;
   return (
     <div class={styles.features}>
-      <h1 id="features">{lang === "en" ? en.features : es.features}</h1>
-      <FeatureImgRight 
-      imgSrc={ImgSummary}
-      title={i18n.feature1Title} 
-      description={i18n.feature1Description} 
-      altImg="tools"
-    />
+      <h1 id="features">{t("features")}</h1>
+      <FeatureImgRight
+        imgSrc={ImgSummary}
+        title={t("feature1Title")}
+        description={t("feature1Description")}
+        altImg="tools"
+      />
       <FeatureImgLeft
-      imgSrc={ImgDetails}
-      title={i18n.feature2Title} 
-      description={i18n.feature2Description} 
-      altImg="details"
-    />
-      <FeatureImgRight 
-      imgSrc={ImgTools}
-      title={i18n.feature3Title} 
-      description={i18n.feature3Description} 
-      altImg="tools"
-    />
+        imgSrc={ImgDetails}
+        title={t("feature2Title")}
+        description={t("feature2Description")}
+        altImg="details"
+      />
+      <FeatureImgRight
+        imgSrc={ImgTools}
+        title={t("feature3Title")}
+        description={t("feature3Description")}
+        altImg="tools"
+      />
       <FeatureImgLeft
-      imgSrc={ImgSummary}
-      title={i18n.feature4Title} 
-      description={i18n.feature4Description} 
-      altImg="content"
-    />
+        imgSrc={ImgSummary}
+        title={t("feature4Title")}
+        description={t("feature4Description")}
+        altImg="content"
+      />
     </div>
   );
 });
